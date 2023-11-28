6d17b9f4ad5131ed598bce78e3fa4c9c2d863daa
kanaxo <60850434+kanaxo@users.noreply.github.com>
2023-11-29 00:06:37 +0800

    Added CSS, submit Todo functionalities

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 "use strict";
 
-const toDoArr = ["Wake up", "Make Coffee", "Brush Teeth"];
+let toDoArr = ["Wake up", "Make Coffee", "Brush Teeth"];
 
 for (const ele of toDoArr) {
 	document.getElementById("toDoList").innerHTML += `<li>${ele}</li>`;
 }
+
+const submitToDo = function () {
+	const taskInput = document.getElementById("Task");
+	const taskInputVal = taskInput.value.trim();
+	console.log(taskInputVal);
+	if (taskInputVal !== "") {
+		toDoArr.push(taskInputVal);
+	}
+	updateToDo();
+	taskInput.value = "";
+};
+
+const updateToDo = function () {
+	const mainList = document.getElementById("toDoList");
+	mainList.innerHTML = "";
+	toDoArr.forEach((Task) => {
+		const listElement = `<li>${Task}</li>`;
+		mainList.innerHTML += listElement;
+	});
+};
 
 console.log(...toDoArr);
