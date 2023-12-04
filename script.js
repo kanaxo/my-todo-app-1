bec4ea157ab5b1efec21fb6859305a863700a34b
kanaxo <60850434+kanaxo@users.noreply.github.com>
2023-12-04 23:26:51 +0800

    added delete functions

--- script.js
+++ script.js
@@ -1,10 +1,40 @@
 "use strict";
 
 let toDoArr = ["Wake up", "Make Coffee", "Brush Teeth"];
+let toDoArrId = [];
 
-for (const ele of toDoArr) {
-	document.getElementById("toDoList").innerHTML += `<li>${ele}</li>`;
-}
+const removeElement = function (e) {
+	// remove element from toDoArr
+	// obtain index
+	const selectedElem = e.target.id; // A1
+	const selectedIndex = selectedElem.slice(1); // 1
+	toDoArr.splice(selectedIndex, 1);
+	toDoArrId.splice(selectedIndex, 1);
+	// update list
+	updateToDo();
+};
+// add event listener for button
+const addDeleteListener = function () {
+	const deleteElems = document.querySelectorAll(
+		toDoArrId.map((id) => `#${id}`).join(",")
+	);
+	console.log(deleteElems);
+	deleteElems.forEach((deleteElem) => {
+		deleteElem.addEventListener("click", removeElement);
+	});
+};
+
+const updateToDo = function () {
+	const mainList = document.getElementById("toDoList");
+	mainList.innerHTML = "";
+	toDoArr.forEach((Task, index) => {
+		const id = `A${index}`;
+		toDoArrId.push(id);
+		const listElement = `<li>${Task}<button id="${id}">Delete</button> </li> `;
+		mainList.innerHTML += listElement;
+	});
+	addDeleteListener();
+};
 
 const submitToDo = function () {
 	const taskInput = document.getElementById("Task");
@@ -17,13 +47,7 @@ const submitToDo = function () {
 	taskInput.value = "";
 };
 
-const updateToDo = function () {
-	const mainList = document.getElementById("toDoList");
-	mainList.innerHTML = "";
-	toDoArr.forEach((Task) => {
-		const listElement = `<li>${Task}</li>`;
-		mainList.innerHTML += listElement;
-	});
-};
-
+// driver code
+updateToDo();
+console.log(toDoArrId);
 console.log(...toDoArr);
